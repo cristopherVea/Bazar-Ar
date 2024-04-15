552553bfd36ae01703a59cf5dc575d88f2db4807
cristopher vea <cristopherveaflores@gmail.com>
2024-04-15 10:41:25 -0700

    nuevo archivo

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Bazar RA" });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About First Node Website" });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "Pagina de contacto" });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Iniciar seccion" });
+});
+
+
+router.get("/conocenos", (req, res) => {
+  res.render("conocenos", { title: "Conoce el proyecto" });
+});
+
+
+export default router;
